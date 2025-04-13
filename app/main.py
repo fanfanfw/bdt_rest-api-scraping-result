cad4e0ecb1d571538e9c06b267acaf5092c791f0
fanfanfw <fanfan.firgiawan69@gmail.com>
2025-04-13 21:47:22 +0700

    update

--- app/main.py
+++ app/main.py
@@ -1,16 +1,6 @@
 from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
 from app.endpoints import router
 from app.dependencies import verify_api_key
 
 app = FastAPI()
-
-# CORS setup
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 app.include_router(router, dependencies=[Depends(verify_api_key)])
